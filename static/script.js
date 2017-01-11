14c88061f4fd9fd13712bbc30d9a2ec9c8f0c5bc
Hasun Amarasekara <hasunamarasekara@live.com>
2017-01-11 00:31:34 +0000

    points correctly redrawing now

--- static/script.js
+++ static/script.js
@@ -13,29 +13,77 @@ function initMap() {
     });
     getter();
 }
-    
+
+
+
+function id_finder(unq_id, element){
+    console.log(element.title, unq_id); 
+    if (element.title == unq_id){
+
+        return true;
+
+    } else{
+
+        return false;
+
+    }
+}
+
 
 function getter(){
    $.ajax({
        url: '/get',
        success: function(data) {
-           // console.log(data.duval[0].lat, data.duval[0].lng);
+           
            if(busses_mrkr.length == 0){
-               bus_mrkrs_len = busses_mrkr.length
+               
+               bus_mrkrs_len = data.duval.length;
                console.log(data.duval.length);
+
                for( var i = 0; i < bus_mrkrs_len; i++){
 
-                   var newLatLng =  { lat: data.duval[i].lat, lng: data.duval[i].lng }         
-                   busses_mrkr.push( new google.Marker({
+                   var newLatLng =  { lat: data.duval[i].lat, lng: data.duval[i].lng };
+                   
+                   busses_mrkr.push( new google.maps.Marker({
                        position: newLatLng,
                        map: map,
                        title: data.duval[i].id
                    }));
+
                }
                console.log(busses_mrkr);
             }
             else {
-                console.log("bus markers length isn't 0");
+                var lst_cnt = 0;
+                var fnd_cnt = 0;
+                for( var i = 0; i < data.duval.length; i++) {
+                    var bus_fnd = busses_mrkr.findIndex(id_finder.bind(null, data.duval[i].id));
+                    console.log( bus_fnd);
+                    
+                    if (bus_fnd != -1) {
+                        busses_mrkr[bus_fnd].setMap(null);
+                        busses_mrkr[bus_fnd] = null;
+                        console.log("drawing new map point");
+
+                        var newLatLng =  { lat: data.duval[i].lat, lng: data.duval[i].lng };
+                         
+                        var u_mrkr = new google.maps.Marker({
+                           position: newLatLng,
+                           map: map,
+                           title: data.duval[i].id
+                        });
+
+                        busses_mrkr[bus_fnd] = u_mrkr;
+                        fnd_cnt += 1;
+
+                   } else {
+
+                       lst_cnt +=1;
+                   }
+
+                    
+                }
+    
             }
                        
        },
